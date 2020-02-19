80935e05fb39b3568422e7762a6f1c0c586aaacf
Julian Olbrich <julianolbrich@gmail.com>
2020-02-19 16:47:15 +0100

    minor update

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,31 @@
 class RestaurantsController < ActionController::Base
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(name: params[:name], address: params[:address], phone_number: params[:phone_number], category: params[:category])
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
 end
+
+  # def edit
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
+
+  # def update
+  #   @restaurant = Restaurant.find(params[:id])
+  #   @restaurant.update(restaurant_params)
+  #   redirect_to restaurant_path(@restaurant)
+  # end
